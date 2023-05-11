85ea2d3dc8ef211c4b924d090a712b1c6c62827b
ivanjhm <101402648+ivanjhm@users.noreply.github.com>
2023-05-11 15:59:15 -0400

    No se que coño pasó
    xd

--- ProyectoDjango/core/urls.py
+++ ProyectoDjango/core/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import agradecimiento, buscador, carrito, celulares, computadores, consolas, ingresarProducto,inicioSesion,olvidoClave,PaginaPrincipal,PovAdmin,Producto1,Producto2,Producto3,Producto4,Producto5,Producto6,Producto7,Producto8,Producto9,Producto10,Producto11,RegistroUsuario,RestablecerContraseña,Usuario
+from .views import agradecimiento, buscador, carrito, celulares, computadores, consolas, ingresarProducto,inicioSesion,olvidoClave,PaginaPrincipal,PovAdmin,Producto1,RegistroUsuario,RestablecerContraseña,Usuario
 
 urlpatterns = [
     path('Recibo', agradecimiento,name="Recibo"),
@@ -12,10 +12,10 @@ urlpatterns = [
     path('Ingresar Producto', ingresarProducto, name="IngresarProducto"),
     path('Inicio seion',inicioSesion, name="InicioSesion"), 
     path('Olvido Clave',olvidoClave, name="OlvidoClave"),
-    path('Pagina Principal',PaginaPrincipal, name="PaginaPrincipal"),
-    path('Administrador',PovAdmin, name="PovAdmin"),
+    path('Pagina ',PaginaPrincipal, name="PaginaPrincipal"),
+    path('',PovAdmin, name="PovAdmin"),
     path('Producto1',Producto1, name="Producto1"),
-    path('',RegistroUsuario, name="RegsitroUsuario"),
+    path('Registro',RegistroUsuario, name="RegistroUsuario"),
     path('Restablecer Contraseña',RestablecerContraseña, name="RestablecerContraseña"),
     path('Usuario',Usuario, name="Usuario"),
 ]

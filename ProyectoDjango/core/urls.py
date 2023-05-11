5eba66394ffd44d4bb162df74f50236d1e3ed916
hksseba <seb.silvan@duocuc.cl>
2023-05-10 21:28:46 -0400

    Css con django
    Agregué todos los links de css con django

--- ProyectoDjango/core/urls.py
+++ ProyectoDjango/core/urls.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import agradecimiento, buscador, carrito
+from .views import agradecimiento, buscador, carrito, celulares, computadores, consolas, ingresarProducto,inicioSesion,olvidoClave,PaginaPrincipal,PovAdmin,Producto1,Producto2,Producto3,Producto4,Producto5,Producto6,Producto7,Producto8,Producto9,Producto10,Producto11,RegistroUsuario,RestablecerContraseña,Usuario
+
 urlpatterns = [
-    path('', agradecimiento,name="Recibo"),
-    path('Productos', buscador,name="Buscar productos"),
-    path('Carrito',carrito, name="carrito")
+    path('Recibo', agradecimiento,name="Recibo"),
+    path('Productos', buscador,name="Productos"),
+    path('Carrito',carrito, name="Carrito"),
+    path('Celulares',celulares, name="Celulares"),
+    path('Computadores',computadores, name="Computadores"),
+    path('Consolas',consolas, name="Consolas"),
+    path('Ingresar Producto', ingresarProducto, name="IngresarProducto"),
+    path('Inicio seion',inicioSesion, name="InicioSesion"), 
+    path('Olvido Clave',olvidoClave, name="OlvidoClave"),
+    path('Pagina Principal',PaginaPrincipal, name="PaginaPrincipal"),
+    path('Administrador',PovAdmin, name="PovAdmin"),
+    path('Producto1',Producto1, name="Producto1"),
+    path('Producto1',Producto2, name="Producto2"),
+    path('Producto1',Producto3, name="Producto3"),
+    path('Producto1',Producto4, name="Producto4"),
+    path('Producto1',Producto5, name="Producto5"),
+    path('Producto1',Producto6, name="Producto6"),
+    path('Producto1',Producto7, name="Producto7"),
+    path('Producto1',Producto8, name="Producto8"),
+    path('Producto1',Producto9, name="Producto9"),
+    path('Producto1',Producto10, name="Producto10"),
+    path('Producto1',Producto11, name="Producto11"),
+    path('',RegistroUsuario, name="RegsitroUsuario"),
+    path('Restablecer Contraseña',RestablecerContraseña, name="RestablecerContraseña"),
+    path('Usuario',Usuario, name="Usuario"),
 ]
--- ProyectoDjango/core/views.py
+++ ProyectoDjango/core/views.py
@@ -13,10 +13,16 @@ def carrito (request):
 def celulares (request):
     return render(request,'core/html/Celulares.html') 
 
-def ingresarProducto (request):
+def computadores (request):
+    return render(request, 'core/html/Computadores.html')
+
+def consolas (request):
+    return render(request, 'core/html/Consolas.html')
+
+def ingresarProducto(request):
     return render(request,'core/html/IngresarProducto.html')
 
-def InicioSesion (request):
+def inicioSesion (request):
     return render(request,'core/html/InicioSesion.html')
 
 def olvidoClave (request):
@@ -30,6 +36,36 @@ def PovAdmin (request):
  
 def Producto1 (request):
     return render(request,'core/html/Producto1.html') 
+
+def Producto2 (request):
+    return render(request,'core/html/Producto2.html') 
+
+def Producto3 (request):
+    return render(request,'core/html/Producto3.html') 
+
+def Producto4 (request):
+    return render(request,'core/html/Producto4.html') 
+
+def Producto5 (request):
+    return render(request,'core/html/Producto5.html') 
+
+def Producto6 (request):
+    return render(request,'core/html/Producto6.html') 
+
+def Producto7 (request):
+    return render(request,'core/html/Producto7.html') 
+
+def Producto8 (request):
+    return render(request,'core/html/Producto8.html') 
+
+def Producto9 (request):
+    return render(request,'core/html/Producto9.html') 
+
+def Producto10 (request):
+    return render(request,'core/html/Producto10.html') 
+
+def Producto11 (request):
+    return render(request,'core/html/Producto11.html') 
  
 def RegistroUsuario (request):
     return render(request,'core/html/RegistroUsuario.html') 

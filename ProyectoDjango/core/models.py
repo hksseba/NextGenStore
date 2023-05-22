ac37762937c721c37782e7d55eb826b030f288f7
GManeiro <ga.maneiro@duocuc.cl>
2023-05-21 22:57:02 -0400

    clave foranea
    Claves foraneas agregadas

--- ProyectoDjango/core/models.py
+++ ProyectoDjango/core/models.py
@@ -21,6 +21,7 @@ class Comuna (models.Model):
     id_comuna  = models.AutoField(primary_key = True, verbose_name='Id de la comuna')
     nombre_comuna = models.CharField(max_length=20, verbose_name='Nombre de la comuna', null = True, blank = False)
     costo_dia = models.IntegerField(max_length=20, verbose_name='costo por dia', null = True, blank = False)
+    region = models.ForeignKey(Region,on_delete=models.CASCADE)
 
 class Producto (models.Model):
     id_producto  = models.AutoField(primary_key = True, verbose_name='Id del producto')
@@ -29,6 +30,7 @@ class Producto (models.Model):
     precio_producto = models.IntegerField(max_length=20, verbose_name='Precio del producto', null = True, blank = False)
     foto_producto = models.ImageField(max_length=20, verbose_name='Imagen del producto', null = True, blank = False)
     stock_producto = models.IntegerField(max_length=20, verbose_name='Stock del producto', null = True, blank = False)
+    categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
 
 class Usuario (models.Model):
 
@@ -40,11 +42,15 @@ class Direccion (models.Model):
     id_direccion  = models.AutoField(primary_key = True, verbose_name='Id de la direccion')
     nombre_direccion = models.CharField(max_length=20, verbose_name='Nombre de la direccion', null = True, blank = False)
     num_direccion = models.IntegerField(max_length=10, verbose_name='Numero de la direccion', null = True, blank = False)
+    comuna = models.ForeignKey(Comuna,on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario,on_delete=models.CASCADE)
 
 class Detalle (models.Model):
     id_detalle  = models.AutoField(primary_key = True, verbose_name='Id de la direccion')
     cantidad = models.IntegerField(max_length=20, verbose_name='Cantidad de productos', null = True, blank = False)
     subtotal = models.IntegerField(max_length=10, verbose_name='Subtotal', null = True, blank = False)
+    producto = models.ForeignKey(Producto,on_delete=models.CASCADE)
+    pedido = models.ForeignKey(Pedido,on_delete=models.CASCADE)
 
 
    
